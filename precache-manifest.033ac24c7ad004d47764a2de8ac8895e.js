209cf11ae5e9dcbc3dab54682b74e5d86e2ba388
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2020-06-18 12:58:27 -0400

    Updates

---
+++ precache-manifest.033ac24c7ad004d47764a2de8ac8895e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d99b9d65d469c1c8c7624869d8d146b2",
+    "url": "/exchange-rate-conversion/index.html"
+  },
+  {
+    "revision": "592a3c06a98a29432218",
+    "url": "/exchange-rate-conversion/static/css/main.50fd4fc1.chunk.css"
+  },
+  {
+    "revision": "1df87bc81c69f508032a",
+    "url": "/exchange-rate-conversion/static/js/2.e4c6d4d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exchange-rate-conversion/static/js/2.e4c6d4d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "592a3c06a98a29432218",
+    "url": "/exchange-rate-conversion/static/js/main.ce6b4e22.chunk.js"
+  },
+  {
+    "revision": "6f936362b9a188b8fbdb",
+    "url": "/exchange-rate-conversion/static/js/runtime-main.794a4674.js"
+  }
+]);
